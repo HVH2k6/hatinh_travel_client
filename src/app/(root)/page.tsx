bb4e898810fe57dd57c60e61a83558e6eca584b3
Windows 11 <hahung09092006@gmail.com>
2025-12-02 12:16:18 +0700

    update code

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,10 +5,13 @@ export default async function page() {
     `${process.env.NEXT_PUBLIC_API_URL}/attractions`,
     { cache: 'no-store' }
   );
-  const resFood = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/food?limit=4`, { cache: 'no-store' });
-  console.log("🚀 ~ page ~ resFood:", resFood)
+  const resFood = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/food?limit=4`,
+    { cache: 'no-store' }
+  );
+
   const dataFood = await resFood.json();
-  console.log("🚀 ~ page ~ dataFood:", dataFood)
+
   const data = await response.json();
 
   return <HaTinhTourism data={data.data} foods={dataFood.data}></HaTinhTourism>;
